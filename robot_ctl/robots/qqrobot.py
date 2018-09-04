49f285603c52cecc8646dde6c8470b7dc2937fd5
netteans <netteans@gmail.com>
2018-09-04 23:33:54 +0800

    test qqbot

--- robot_ctl/robots/qqrobot.py
+++ robot_ctl/robots/qqrobot.py
@@ -6,17 +6,48 @@ loginBots = {}
 
 logger = logging.getLogger(__name__)
 
-def loginQQ(qqlist):
-    for qq in qqlist:
-        bot = QQBot()
-        bot.Login(['-q', qq])
-        loginBots[qq] = bot;
+
+def onInit(bot):
+    logger.debug('%s.onInit', __name__)
 
 
-def getGroups():
+def onQrcode(bot, pngPath, pngContent):
+    logger.debug('%s.onQrcode: %s (%d bytes)', __name__, pngPath, len(pngContent))
+
+
+def onQQMessage(bot, contact, member, content):
+    if content == '--version' and getattr(member, 'uin') == bot.conf.qq:
+        bot.SendTo(contact, 'QQbot-' + bot.conf.version)
+
+
+def onInterval(bot):
+    logger.debug('%s.onInterval', __name__)
+
+
+def onStartupComplete(bot):
+    logger.debug('%s.onStartupComplete', __name__)
+
+
+def onUpdate(bot, tinfo):
+    logger.debug('%s.onUpdate: %s', __name__, tinfo)
+
+
+def onPlug(bot):
+    logger.debug('%s.onPlug', __name__)
+
+
+def onUnplug(bot):
+    logger.debug('%s.onUnplug', __name__)
+
+
+def onExit(bot, code, reason, error):
+    logger.debug('%s.onExit: %r %r %r', __name__, code, reason, error)
+
+
+def getGroups(groupname=None):
     groups = {}
     for qq in loginBots:
-        groups[qq] = loginBots[qq].List('group')
+        groups[qq] = loginBots[qq].List('group', groupname)
 
     return groups
 
@@ -30,5 +61,17 @@ def sendToGroup(message):
                 bot.SendTo(group, message)
 
 
-def onStartupComplete(bot):
-    logger.info('logged in')
+def loginQQ(qqlist):
+    for qq in qqlist:
+        bot = QQBot()
+        bot.AddSlot(onInit)
+        bot.AddSlot(onQrcode)
+        bot.AddSlot(onQQMessage)
+        bot.AddSlot(onInterval)
+        bot.AddSlot(onStartupComplete)
+        bot.AddSlot(onUpdate)
+        bot.AddSlot(onPlug)
+        bot.AddSlot(onUnplug)
+        bot.AddSlot(onExit)
+        bot.Login(['-q', qq])
+        loginBots[qq] = bot
--- test/test.py
+++ test/test.py
@@ -2,10 +2,28 @@ import logging
 
 from robot_ctl.robots.qqrobot import loginQQ, getGroups, sendToGroup
 
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] - %(name)s:[%(lineno)d] - %(levelname)s - %(message)s')
 
-loginQQ(['3283253806'])
+logger = logging.getLogger(__name__)
+
+# loginQQ(['3283253806'])
+
+loginQQ(['1411729768'])
+
+key = ''
+while key != 'c':
+    key = raw_input('wait ...')
+    logger.debug(key)
+    logger.info(key)
+    logger.warn(key)
+    logger.error(key)
+    logger.fatal(key)
+    logger.critical(key)
 
 print(getGroups())
+# sendToGroup('for test')
 
-sendToGroup('for test')
+key = ''
+while key != 'q':
+    key = raw_input('wait ...')
+    print(key)
